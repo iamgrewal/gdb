5bf49f980c0bf0a6613f983ce95621e032937c64
John Mark Suhy <jmsuhy@igovsol.com>
2021-01-25 14:34:48 -0500

    Initial 3.5.26 commit

---
+++ README.md
@@ -0,0 +1,6 @@
+GraphStack.io GDB Plugin for Neo4j.
+
+The build system creates the full graphstack.io gdb distro as well as a plugin 
+which is being tested that can turn on enterprise features inside an existing community edition of the same version number.
+
+
---
+++ cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/AggregationAcceptanceTest.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j Enterprise Edition. The included source
+ * code can be redistributed and/or modified under the terms of the
+ * GNU AFFERO GENERAL PUBLIC LICENSE Version 3
+ * (http://www.fsf.org/licensing/licenses/agpl-3.0.html) with the
+ * Commons Clause, as found in the associated LICENSE.txt file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * Neo4j object code can be licensed independently from the source
+ * under separate terms from the AGPL. Inquiries can be directed to:
+ * licensing@neo4j.com
+ *
+ * More information is also available at:
+ * https://neo4j.com/licensing/
+ */
+package org.neo4j.internal.cypher.acceptance
+
+import org.neo4j.cypher.{EnterpriseRunWithConfigTestSupport, ExecutionEngineFunSuite}
+import org.neo4j.internal.cypher.acceptance.comparisonsupport.Configs
+import org.neo4j.internal.cypher.acceptance.comparisonsupport.CypherComparisonSupport
+import org.neo4j.values.storable.DurationValue
+
+
+class AggregationAcceptanceTest extends ExecutionEngineFunSuite with CypherComparisonSupport {// with EnterpriseRunWithConfigTestSupport {
+
+  private val INTERPRETED_34_35 = Configs.InterpretedAndSlotted - Configs.Version3_1 - Configs.Version2_3
+
+  // Non-deterministic query -- needs TCK design
+  test("should aggregate using as grouping key expressions using variables in scope and nothing else") {
+    val userId = createLabeledNode(Map("userId" -> 11), "User")
+    relate(userId, createNode(), "FRIEND", Map("propFive" -> 1))
+    relate(userId, createNode(), "FRIEND", Map("propFive" -> 3))
+    relate(createNode(), userId, "FRIEND", Map("propFive" -> 2))
+    relate(createNode(), userId, "FRIEND", Map("propFive" -> 4))
+
+    val query1 = """MATCH (user:User {userId: 11})-[friendship:FRIEND]-()
+                   |WITH user, collect(friendship)[toInt({param} * count(friendship))] AS selectedFriendship
+                   |RETURN id(selectedFriendship) AS friendshipId, selectedFriendship.propFive AS propertyValue""".stripMargin
+    val query2 = """MATCH (user:User {userId: 11})-[friendship:FRIEND]-()
+                   |WITH user, collect(friendship) AS friendships
+                   |WITH user, friendships[toInt({param} * size(friendships))] AS selectedFriendship
+                   |RETURN id(selectedFriendship) AS friendshipId, selectedFriendship.propFive AS propertyValue""".stripMargin
+    val params = Map("param" -> 3)
+
+    val result1 = executeWith(Configs.InterpretedAndSlotted, query1, params = params).toList
+    val result2 = executeWith(Configs.InterpretedAndSlotted, query2, params = params).toList
+
+    result1.size should equal(result2.size)
+  }
+
+
+}
